8ab83a5f1412bd01c4bea8199dba3cdd196efcc5
Edmund Neil Quiros <edmund.neil@tigo.co.cr>
2018-03-14 18:22:28 -0600

    Trabajo

--- test/ui.r
+++ test/ui.r
@@ -1,77 +1,75 @@
+library(shiny)
 
-library('shiny')
-#runGitHub(repo = 'shiny_geolocation', username = 'AugustT', subdir = 'simple_example')
-
-shinyUI(
-  fluidPage(
-    titlePanel("Using Geolocation"),
-    
-    tags$script(
-      '$(document).ready(function() {
-      
-      function getLocation(callBack){
-      var options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-      };
-      
-      navigator.geolocation.getCurrentPosition(onSuccess,onError);
-      
-      
-      function onError (err) {
-      Shiny.onInputChange("geolocation", false);
-      }
-      
-      function onSuccess (position) {
-      setTimeout(function () {
-      var coords = position.coords;
-      var timestamp = new Date();
-      
-      console.log(coords.latitude + ", " + coords.longitude, "," + coords.accuracy);
-      Shiny.onInputChange("geolocation", true);
-      Shiny.onInputChange("lat", coords.latitude);
-      Shiny.onInputChange("lon", coords.longitude);
-      Shiny.onInputChange("accuracy", coords.accuracy);
-      Shiny.onInputChange("time", timestamp)
-      
-      console.log(timestamp);
-      
-      if(callback){
-      callback();
-      }
-      },1100)
-      }
-      }
-      var TIMEOUT = 1000;
-      var started = false;
-      
-      function getLocationRepeat(){
-      if (!started) {
-      started = true;
-      getLocation(getLocationRepeat);
-      return;
-      }
-      setTimeout(function(){
-      
-      getLocation(getLocationRepeat);
-      }, TIMEOUT);
-      };
-      getLocationRepeat();
-      });
-      '
-    ),
-    
-    #show a plot of the generated distribution
-    fluidRow(
-      column(
-        width = 2,
-        verbatimTextOutput("lat"),
-        verbatimTextOutput("lon"),
-        verbatimTextOutput("geolocation"),
-        verbatimTextOutput("accuracy"),
-        verbatimTextOutput("time")
-      )
-    )
+shinyUI(fluidPage(
+  
+  titlePanel("Using Geolocation"),
+  
+  tags$script('
+              $(document).ready(function () {
+              
+              function getLocation(callback){
+              var options = {
+              enableHighAccuracy: true,
+              timeout: 5000,
+              maximumAge: 0
+              };
+              
+              navigator.geolocation.getCurrentPosition(onSuccess, onError);
+              
+              function onError (err) {
+              Shiny.onInputChange("geolocation", false);
+              }
+              
+              function onSuccess (position) {
+              setTimeout(function () {
+              var coords = position.coords;
+              var timestamp = new Date();
+              
+              console.log(coords.latitude + ", " + coords.longitude, "," + coords.accuracy);
+              Shiny.onInputChange("geolocation", true);
+              Shiny.onInputChange("lat", coords.latitude);
+              Shiny.onInputChange("long", coords.longitude);
+              Shiny.onInputChange("accuracy", coords.accuracy);
+              Shiny.onInputChange("time", timestamp)
+              
+              console.log(timestamp);
+              
+              if (callback) {
+              callback();
+              }
+              }, 1100)
+              }
+              }
+              
+              var TIMEOUT = 1000; //SPECIFY
+              var started = false;
+              function getLocationRepeat(){
+              //first time only - no delay needed
+              if (!started) {
+              started = true;
+              getLocation(getLocationRepeat);
+              return;
+              }
+              
+              setTimeout(function () {
+              getLocation(getLocationRepeat);
+              }, TIMEOUT);
+              
+              };
+              
+              getLocationRepeat();
+              
+              });
+              '),
+  
+  # Show a plot of the generated distribution
+  fluidRow(column(width = 5,
+                  verbatimTextOutput("lat"),
+                  verbatimTextOutput("long"),
+                  verbatimTextOutput("geolocation"),
+                  verbatimTextOutput("accuracy"),
+                  verbatimTextOutput("time"))
+           
+  )
   )
 )
